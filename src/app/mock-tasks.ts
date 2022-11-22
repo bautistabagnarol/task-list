7c7db79aa75767898dbeab3a46ec8b6b19a6f41a
bautistabagnarol <bautabagnarol@gmail.com>
2022-11-22 11:59:15 -0300

    Agregar mock de tareas y el interface de la misma

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,26 @@
+export const TASKS =[
+{
+    id: 1,
+    text:"Terminar primer modulo de Angular",
+    dat:"Agosto 5 a las 12:00",
+    reminder: true
+},
+{
+    id: 2,
+    text:"Hacer las compras para la cena",
+    dat:"Agosto 5 a las 17:00",
+    reminder: true
+},
+{
+    id: 3,
+    text:"Investigas sobre Boostrap",
+    dat:"Agosto 5 a las 19:00",
+    reminder: true
+},
+{
+    id: 4,
+    text:"Leer mi libro favorito",
+    dat:"Agosto 5 a las 21:00",
+    reminder: true
+},
+]
