02b574959cce9d3ed9b875833012c9bdf99b925f
bautistabagnarol <bautabagnarol@gmail.com>
2022-12-16 11:53:35 -0300

    componente de lista de items

--- src/app/Task.ts
+++ src/app/Task.ts
@@ -1,5 +1,5 @@
 export interface Task {
-    id?: number, //Cuando creamos podria no venir
+    id?: number;
     text: string;
     day: string;
     reminder: boolean;
--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -16,12 +16,12 @@ export const TASKS: Task[] = [
     id: 3,
     text:"Investigas sobre Boostrap",
     day:"Agosto 5 a las 19:00",
-    reminder: true
+    reminder: false
 },
 {
     id: 4,
     text:"Leer mi libro favorito",
     day:"Agosto 5 a las 21:00",
-    reminder: true
+    reminder: false
 },
 ]
